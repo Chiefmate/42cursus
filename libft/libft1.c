010697f7bb4fab70ef829a157a67428f00e7437d
hyunhole <hyunhole@student.42seoul.kr>
2021-11-20 15:08:12 +0900

    libft1 functions name modified

--- libft/libft1.c
+++ libft/libft1.c
@@ -10,22 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int isalpha(int c)
+int ft_isalpha(int c)
 {
     return (((unsigned)c | 32) - 'a' < 26);
 }
 
-int isdigit(int c)
+int ft_isdigit(int c)
 {
     return ((unsigned)c - '0' < 10);
 }
 
-int isalnum(int c)
+int ft_isalnum(int c)
 {
-    return (isalpha(c) || isdigit(c));
+    return (ft_isalpha(c) || ft_isdigit(c));
 }
 
-int isascii(int c)
+int ft_isascii(int c)
 {
     if (0 <= c && c <= 127)
         return (1);
@@ -33,7 +33,7 @@ int isascii(int c)
         return (0);
 }
 
-int isprint(int c)
+int ft_isprint(int c)
 {
     return ((unsigned)c - 32 < 95);
 }
