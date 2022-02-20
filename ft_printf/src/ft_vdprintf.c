96b38f778b1ddca49971aad26f9e992a58f3d93a
hyunhole <hyunhole@student.42seoul.kr>
2022-02-20 20:24:40 +0900

    wow

--- ft_printf/src/ft_vdprintf.c
+++ ft_printf/src/ft_vdprintf.c
@@ -12,6 +12,39 @@
 
 #include "../include/ft_printf.h"
 
+static int	ft_process_longlong(int fd, unsigned long long value, char c)
+{
+	int	ret;
+
+	ret = 0;
+	if (c == 'c')
+		ret += ft_putchar_fd(fd, (char)value);
+	else if (c == 'x')
+		ret += ft_puthexsml_fd(fd, (long long)value);
+	else if (c == 'X')
+		ret += ft_puthexbig_fd(fd, (long long)value);
+	else if (c == 'p')
+	{
+		ret += ft_putstr_fd(fd, "0x");
+		ret += ft_puthex_fd(fd, (long long)value, 'x');
+	}
+	return (ret);
+}
+
+static int	ft_process_int(int fd, unsigned int value, char c)
+{
+	int	ret;
+
+	ret = 0;
+	if (c == 'c')
+		ret += ft_putchar_fd(fd, (char)value);
+	else if (c == 'd' || c == 'i')
+		ret += ft_putnbr_fd(fd, (int)value);
+	else if (c == 'u')
+		ret += ft_putnbr_fd(fd, value);
+	return (ret);
+}
+
 int	ft_vdprintf(int fd, const char *fmt, va_list ap)
 {
 	int		ret;
@@ -23,23 +56,12 @@ int	ft_vdprintf(int fd, const char *fmt, va_list ap)
 		{
 			if (*(++fmt) == '%')
 				ret += ft_putchar_fd(fd, *fmt);
-			else if (*fmt == 'c')
-				ret += ft_putchar_fd(fd, (char)va_arg(ap, int));
+			else if (*fmt == 'c' || *fmt == 'd' || *fmt == 'i' || *fmt == 'u')
+				ret += ft_process_int(fd, (unsigned int)va_arg(ap, int), *fmt);
 			else if (*fmt == 's')
 				ret += ft_putstr_fd(fd, va_arg(ap, char *));
-			else if (*fmt == 'p')
-			{
-				ret += ft_putstr_fd(fd, "0x");
-				ret += ft_puthex_fd(fd, va_arg(ap, long long), 'x');
-			}
-			else if (*fmt == 'd' || *fmt == 'i')
-				ret += ft_putnbr_fd(fd, va_arg(ap, int));
-			else if (*fmt == 'u')
-				ret += ft_putnbr_fd(fd, va_arg(ap, unsigned int));
-			else if (*fmt == 'x')
-				ret += ft_puthexsml_fd(fd, va_arg(ap, long long));
-			else if (*fmt == 'X')
-				ret += ft_puthexbig_fd(fd, va_arg(ap, long long));
+			else if (*fmt == 'x' || *fmt == 'X' || *fmt == 'p')
+				ret += ft_process_longlong(fd, (unsigned long long)va_arg(ap, long long), *fmt);
 		}
 		else
 			ret += ft_putchar_fd(fd, *(fmt++));
