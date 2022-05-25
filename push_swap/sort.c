88ccc1e00ab39259e1c60a51807eb6f474d9e605
hyunhole <hyunhole@student.42seoul.kr>
2022-05-25 20:57:09 +0900

    needs sort implementation

---
+++ push_swap/sort.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunhole <hyunhole@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 20:40:26 by hyunhole          #+#    #+#             */
+/*   Updated: 2022/05/25 20:40:26 by hyunhole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	A_to_B(t_stack *a, t_stack *b, int r)
+{
+	int	lp;
+	int	rp;
+
+	lp = select_lp(t_stack *a, r);
+	rp = select_rp(t_stack *a, r);
+}
+
+/*	avl tree에 저장
+ */
+int	select_lp(t_stack *s, int r)
+{
+
+}
+
+int	select_rp(t_stack *s, int r)
+{
+	
+}
